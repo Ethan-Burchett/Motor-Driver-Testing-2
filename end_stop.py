c91ba7e71699833b8e28dd8af776d478e7370952
Ethan <ethanburchett@gmail.com>
2023-04-18 16:21:23 -0700

    working pins mid april

--- end_stop.py
+++ end_stop.py
@@ -1,5 +1,5 @@
 # using gpio 2
-
+#working as of mid April 
 #working with end stop limit switch with signal(green) on pin 19 using GPIO.BOARD
 #red: voltage(4), black:ground (6), green:signal(19)
 button = 0
@@ -16,3 +16,4 @@ GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 GPIO.add_event_detect(19,GPIO.FALLING,callback=button_callback,bouncetime=50) # Setup event on pin 10 rising edge
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
+
